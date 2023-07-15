6dc640dc64970d3adeb18f420bf330fdf7bd0d89
shinuamai <laurad00sandovalg@gmail.com>
2023-07-15 14:20:43 -0500

    remove contacto and readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
+  { path: 'inicio', component: HomeComponent},
   { path: 'posts', component: PostListComponent }
 ];
 
